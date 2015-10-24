4ad038fc1d10ba1e23b8d259f7b11b0026dfacf8
ChiefOfGxBxL <chiefofgxbxl@github.com>
2015-10-23 20:05:50 -0400

    Table tabbing, project renamed
    Project was renamed from Table.js to Spreadsheet.js to be more clear on
    what the library does. Also has the added benefit of avoiding ambiguity
    between the Table object, and the html table it contained.
    
    User can now tab through cells, which will select the next cell after
    the one which is currently focused. It overflows to the next row, if
    necessary.
    
    Focusing on a cell now selects all of its text for easier editing.

--- Spreadsheet.js
+++ Spreadsheet.js
@@ -1,14 +1,14 @@
-/*
-	Table.js (v0.7)
+/*!
+	Spreadsheet.js (v0.8)
 	by Greg Lang
 	
 	A simple javascript library to easily create
 	and utilize tables that act as worksheets.
 
-	https://github.com/ChiefOfGxBxL/Table.js
+	https://github.com/ChiefOfGxBxL/Spreadsheet.js
 */
 
-function Table(ctx,row,col) {
+function Spreadsheet(ctx,row,col) {
 	// Public
 	this.name = ctx.getAttribute('name');
 	this.table = document.createElement("table"); // defined below by Initialization
@@ -21,10 +21,11 @@ function Table(ctx,row,col) {
 	var _colCount = col;
 	var _rowCounter = 0;
 	var _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var _this = this; // set to this object; allows functions to bypass functional scope and access the Spreadsheet
 	
 	
 	
-	// Protected methods
+	// Protected functions
 	function tdDblClick(e) {
 		e.target.contentEditable = true;
 		e.target.focus();
@@ -35,19 +36,81 @@ function Table(ctx,row,col) {
 	}
 	function tdClick(e) {
 		// remove cellFocus class from all other cells that may have this class
-		var cellFocusNodes = (e.target.parentNode.parentNode).querySelectorAll("td.cellFocus"); // grand-parent of <td> is <table>
-		for(var i = 0; i < cellFocusNodes.length; i++) {
-			cellFocusNodes[i].className = cellFocusNodes[i].className.replace(/cellFocus/,'').trim();
-		}
+		_this.unfocusCells();
+		_this.focusCell(e.target);
+	}
+	function tdKeyPress(e) {
+		console.log(e); // DEBUG
 		
-		e.target.className += "cellFocus";
+		if(e.key == "Enter" || e.keyCode == 13) {
+			e.preventDefault();
+			e.target.blur();
+			deselectAllText();
+		}
+		else if(e.key == "Tab" || e.keyCode == 9) {
+			/* select the cell to the right of this one
+			else if end of line, go to the next line */
+			e.preventDefault();
+			e.target.blur();
+			deselectAllText();
+			
+			// move on to next cell
+			var colOfTable = e.target.cellIndex;
+			var rowOfTable = e.target.parentElement.rowIndex;
+			
+			/* if we tab from the top-left most content cell,
+			we are at [1,1], so now move to [1,2] if possible */
+			var rowCount = _this.getRowCount();
+			var colCount = _this.getColCount();
+			
+			if(rowOfTable == rowCount && colOfTable == colCount) {
+				// We are on the last cell, and thus cannot tab to a next cell
+				return;
+			}
+			else {
+				if(colOfTable < colCount) {
+					// can select cell in next column
+					var nextCell = _this.selectCell(rowOfTable - 1, colOfTable);
+					_this.focusCell(nextCell);
+				}
+				else {
+					// overflow on column, go to next row starting at the 1st column
+					var nextCell = _this.selectCell(rowOfTable, 0);
+					_this.focusCell(nextCell);
+				}
+			}
+		}
+	}
+	function deselectAllText() {
+		if(document.body.createTextRange) {
+			// TODO: ms
+		}
+		else if(window.getSelection) {
+			var selection = window.getSelection(); 
+			selection.removeAllRanges();
+		}
+	}
+	function selectText(element) {
+		/*	Special thanks to Eswar Rajesh Pinapala for this cross-browser snippet
+			Reference: https://stackoverflow.com/questions/11128130/select-text-in-javascript
+		*/
+		if (document.body.createTextRange) { // ms
+			var range = document.body.createTextRange();
+			range.moveToElementText(element);
+			range.select();
+		} else if (window.getSelection) { // moz, opera, webkit
+			var selection = window.getSelection();            
+			var range = document.createRange();
+			range.selectNodeContents(element);
+			selection.removeAllRanges();
+			selection.addRange(range);
+		}
 	}
 	
 	
 	
 	// Methods
 	this.addRow = function() {
-		
 		var tr = document.createElement("tr");
 		
 		var td = document.createElement("td");
@@ -63,6 +126,7 @@ function Table(ctx,row,col) {
 			td.ondblclick = tdDblClick;
 			td.onblur = tdBlur;
 			td.onclick = tdClick;
+			td.onkeypress = tdKeyPress;
 			
 			tr.appendChild(td);
 		}
@@ -73,7 +137,7 @@ function Table(ctx,row,col) {
 		return;
 	};
 	this.addCol = function() {
-		if(_colCount == 702) return; // Table.js can go up to column ZZ = 26^2 + 26
+		if(_colCount == 702) return; // Spreadsheet.js can go up to column ZZ = 26^2 + 26
 		var newTh = document.createElement("th");
 		newTh.innerHTML = numToLetterBase(++_colCount);
 		this.table.children[0].appendChild(newTh);
@@ -87,6 +151,7 @@ function Table(ctx,row,col) {
 			newTd.ondblclick = tdDblClick;
 			newTd.onblur = tdBlur;
 			newTd.onclick = tdClick;
+			newTd.onkeypress = tdKeyPress;
 			
 			this.table.children[i].appendChild(newTd);
 		}
@@ -97,20 +162,35 @@ function Table(ctx,row,col) {
 	}
 	this.selectCell = function(row,col) {
 		// selectCell(0,0) -> table.children[1].children[1]
+		return this.table.children[row+1].children[col+1];
+	}
+	this.cellContent = function(row,col) {
 		return this.table.children[row+1].children[col+1].textContent;
 	}
 	this._ = function(cellname) {
 		var cell = parseCellname(cellname);
-		if(cell[1].length > 2) return; // Table.js only supports cellnames up to ZZ (which is 702 columns!)
+		if(cell[1].length > 2) return; // Spreadsheet.js only supports cellnames up to ZZ (which is 702 columns!)
 		
 		// ex. [A,4] or [ZZ,210]
 		var letterPart = letterBaseToNum(cell[0]);
 		var digitPart = cell[1];
 		
-		return this.selectCell(digitPart-1,letterPart-1);
+		return this.cellContent(digitPart-1,letterPart-1);
 	}
 	//this.tabulateData = function() {} // output data in a useful form, ex. CSV
-	
+	this.focusCell = function(cellElement) {
+		cellElement.className += " cellFocus";
+		cellElement.contentEditable = true;
+		cellElement.focus();
+		
+		selectText(cellElement);
+	}
+	this.unfocusCells = function() {
+		var cellFocusNodes = (_this.table).querySelectorAll("td.cellFocus"); // grand-parent of <td> is <table>
+		for(var i = 0; i < cellFocusNodes.length; i++) {
+			cellFocusNodes[i].className = cellFocusNodes[i].className.replace(/cellFocus/,'').trim();
+		}
+	}
 	
 	
 	// Accessors and Mutators
@@ -170,7 +250,6 @@ function Table(ctx,row,col) {
 
 		return (alphabet[x[0]] + alphabet[x[1]]).trim();
 	}
-
 	function letterBaseToNum(s) {
 		if(s.length > 2) return;
 		
@@ -179,17 +258,15 @@ function Table(ctx,row,col) {
 		
 		return alphabet.indexOf(s.substr(0,1))*26 + alphabet.indexOf(s.substr(1))
 	}
-	
-	/* parseCellname(c)
-		Takes a cell name c, and splits it up into a duple containing the
-		'letterPart' and the 'numberPart'.
-		
-		Ex. A7 -> [A,7]
-			G15 -> [G,15]
-	*/
 	function parseCellname(c) {
+		/* parseCellname(c)
+			Takes a cell name c, and splits it up into a duple containing the
+			'letterPart' and the 'numberPart'.
+			
+			Ex. A7 -> [A,7]
+				G15 -> [G,15]
+		*/
 		var index = c.search(/\d/); // Finds first occurrence of digit
-		
 		return [c.substr(0,index), c.substr(index)];
 	}
 	
