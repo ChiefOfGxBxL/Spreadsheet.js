03125fae04d5afcca941fd7a81e443b916dc1aef
ChiefOfGxBxL <chiefofgxbxl@github.com>
2016-09-25 12:39:35 -0400

    New gh-pages created with readme.md and modified theme

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,10 +7,7 @@ window.addEventListener('load', function() {
         myTable = new Spreadsheet({
             rows: row,
             cols: col,
-            context: container,
-            onCellValueChanged: function(cell, old, newv) {
-                console.log(old + '->' + newv);
-            }
+            context: container
         });
     }
     
@@ -20,6 +17,36 @@ window.addEventListener('load', function() {
         generateTable(rowInput.value, colInput.value);
     });
     
+    generateExamples();
+    
     // Page on load
     generateTable(6, 8);
 });
+
+function generateExamples() {
+    // Basic example
+    var container = document.getElementById("basicSpreadsheet"); // have a div handy to hold the table
+    myTable = new Spreadsheet({
+        rows: 5,
+        cols: 10,
+        context: container
+    });
+    
+    // Pre-populated spreadsheet
+    var tableData = new Spreadsheet({
+        context: document.getElementById('tableDataContainer'),
+        data: [[1,3,5], [2,4,6], [10,20,30,40]]
+    });
+    
+    // Event handlers
+    var log = document.getElementById('log');
+    var eventTable = new Spreadsheet({
+        context: document.getElementById('eventSpreadsheet'),
+        rows: 5,
+        cols: 5,
+        autofill: true,
+        onCellValueChanged: function(cell, oldValue, newValue) {
+            log.innerHTML += 'Value changed: ' + oldValue + ' -> ' + newValue + '\n';
+        }
+    });
+}
